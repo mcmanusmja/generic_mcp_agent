128d125ca47e8e1ad7d023c50605735b03acb71d
mcmanusmja <mick@timelesstoday.io>
2025-10-22 15:32:16 -0700

    Added langchain runnable

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
 
 from agents.responder.agent import ResponderAgent
+from agents.responder.agent import ResponderAgentRunnable
 from schemas.message import MCPMessage
 
 
@@ -43,6 +44,23 @@ def main():
     agent = ResponderAgent()
     response = agent(message)
     print(response.model_dump_json(indent=2))
+
+    runnable = ResponderAgentRunnable()
+
+    test_input = {
+        "sender": "planner",
+        "receiver": "responder",
+        "type": "task_request",
+        "payload": {
+            "task_id": "T-808",
+            "objective": "Summarize the objective",
+            "format": "html"
+
+        }
+    }
+
+    output = runnable.invoke(test_input)
+    print(f"output from runnable is: {output}")
 
 
 if __name__ == "__main__":
